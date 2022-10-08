1edaa48f932a4429471a20d13c675ca1feaa2eb0
gyub99 <kimgubin@naver.com>
2022-10-08 21:11:03 +0900

    Spring Boot 초기 환경 셋팅

---
+++ src/main/java/com/example/olivebookserver/OliveBookServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.olivebookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveBookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliveBookServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/olivebookserver/OliveBookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olivebookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveBookServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
